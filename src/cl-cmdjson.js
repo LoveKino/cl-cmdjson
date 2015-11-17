a65d13320b4eabac8600f29713d549febcc63244
ddchen <842914439@qq.com>
2015-11-17 16:22:59 +0800

    base

--- src/cl-cmdjson.js
+++ src/cl-cmdjson.js
@@ -1,1 +1,73 @@
-module.exports = () => {}
+/**
+ * grammer
+ *
+ * ["$[command]", ...args]
+ *
+ * ["$[add]"]
+ * 
+ */
+
+module.exports = (cmdMap, opts) => {
+    opts = merge({
+        cmdNameReg: /^\$\[(.*)\]$/
+    }, opts)
+    checkCmdMap(cmdMap);
+
+    let getValue = (jsonCmd) => {
+        let res = parseJsonCmd(opts.cmdNameReg, jsonCmd);
+        if (res === false) {
+            return jsonCmd;
+        } else {
+            let {
+                cmdName, args
+            } = res;
+            let fun = cmdMap[cmdName];
+            if (!fun)
+                throw new Error(`Missing function definition for cmd ${cmdName}`);
+            for (let i = 0; i < args.length; i++) {
+                args[i] = getValue(args[i]);
+            }
+            return fun.apply(undefined, args);
+        }
+    }
+
+    return getValue;
+}
+
+let parseJsonCmd = (cmdNameReg, jsonCmd) => {
+    if (!isArray(jsonCmd))
+        return false;
+
+    let cmdName = jsonCmd[0];
+    if (typeof cmdName !== "string")
+        return false;
+
+    let arr = cmdNameReg.exec(cmdName);
+
+    if (!arr)
+        return false;
+
+    return {
+        cmdName: arr[1],
+        args: jsonCmd.slice(1)
+    };
+}
+
+let checkCmdMap = (cmdMap) => {
+    if (!cmdMap || typeof cmdMap !== 'object')
+        throw new Error(`Expect object for cmdMap. ` + cmdMap);
+    for (let name in cmdMap) {
+        let v = cmdMap[name];
+        if (typeof v !== 'function')
+            throw new Error(`Expect function type for cmdMap's attribute ${name}.`);
+    }
+}
+
+let isArray = v => v && typeof v === 'object' && typeof v.length === 'number';
+
+let merge = (def, map = {}) => {
+    for (let name in map) {
+        def[name] = map[name];
+    }
+    return def;
+}
--- test/base.js
+++ test/base.js
@@ -1,8 +1,33 @@
 import assert from "assert";
-import index from "../index";
+import CmdJson from "../index";
 
 describe("base", () => {
-  it("base", () => {
+    it("base", () => {
+        let cmdJson = CmdJson({
+            add: (x, y) => x + y
+        });
 
-  });
+        assert.equal(cmdJson(["$[add]", 1, 2]), 3);
+    });
+
+    it("compose", () => {
+        let cmdJson = CmdJson({
+            add: (x, y) => x + y,
+            sub: (x, y) => x - y,
+        });
+
+        assert.equal(cmdJson(["$[add]", ["$[sub]", 9, 2], 1]), 8);
+    });
+
+    it("normal", () => {
+        let cmdJson = CmdJson({
+            add: (x, y) => x + y
+        });
+
+        assert.equal(cmdJson(12), 12);
+        assert.equal(cmdJson("ok"), "ok");
+        assert.equal(cmdJson({
+            v: 1
+        }).v, 1);
+    });
 });
